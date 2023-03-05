ef24cc9405bfd0433394ea31c7314cfa3c8e9138
Paul H. Liu <paul@thev.net>
2023-03-04 16:34:05 -0800

    Upgrade to dfx 0.12.1

--- shell.nix
+++ shell.nix
@@ -1,8 +1,13 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 let
-  ic-utils = import (builtins.fetchGit {
-    url = "https://github.com/ninegua/ic-utils";
-    rev = "6e9b645e667fb59f51ad8cfa40e2fd7fdd7e52d0";
-    ref = "refs/heads/main";
-  }) { inherit pkgs; };
-in pkgs.mkShell { nativeBuildInputs = [ ic-utils pkgs.wasmtime pkgs.pandoc pkgs.nodejs pkgs.nodePackages.prettier ]; }
+  dfx-env = import (fetchTarball
+    "https://github.com/ninegua/ic-nix/releases/download/20230101/dfx-env.tar.gz")
+    { };
+in dfx-env.overrideAttrs (old: {
+  nativeBuildInputs = old.nativeBuildInputs ++ [
+    pkgs.wasmtime
+    pkgs.pandoc
+    pkgs.nodejs
+    pkgs.nodePackages.prettier
+  ];
+})
